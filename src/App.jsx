12608c3b2e5baec0a4d4dd4191438cb999caab66
sushantbro11 <sushantbro12@gmail.com>
2024-01-12 03:21:12 +0545

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import NavBar from './components/NavBar';
 
-
 const App = () => {
-
+  
   return (
+ 
     <div className="w-full min-h-screen h-full bg-gradient-to-b from-gray-800 to-black text-white">
+      
       <NavBar />
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+
 import HeroImage from "../assets/2.jpg";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 const Home = () => {
   const handleButtonClick = () => {
@@ -13,17 +14,15 @@ const Home = () => {
     <div className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full p-20 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-6xl font-bold text-white pt-14">Hey, I'm Sushant</h2>
+          <h2 className="text-4xl sm:text-5xl font-bold text-white pt-14 mt-32 typing-demo ">Hey, I'm Sushant</h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and designing software.
-            Currently, I love to work on web applications using technologies like
-            React, Tailwind, Next JS, and GraphQL.
+          As an aspiring web developer, I'm committed to a constant learning journey, staying aware of the latest trends to craft modern, user-friendly websites. Eager for opportunities to apply and expand my skills.
           </p>
-          <button onClick={handleButtonClick} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center w-40">
+          <button onClick={handleButtonClick} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 my-4 rounded inline-flex items-center w-40">
             <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
             <span>Download CV</span>
           </button>
-          <div className='text-4xl py-4 flex justify-left gap-5 text-gray-600'>
+          <div className='text-4xl py-4 mt-2 flex justify-left gap-5 text-gray-600'>
             <Link to="https://np.linkedin.com/in/sushant-tripathee-14b412209" className="hover:scale-150 transition-transform duration-300 ease-in-out">
               <FaLinkedin />
             </Link>
@@ -35,11 +34,13 @@ const Home = () => {
             </Link>
           </div>
         </div>
+        
         <img
-          src={HeroImage}
-          alt="my profile"
-          className="rounded-3xl w-80 h-80 px-3 ml-8"
-        />
+  src={HeroImage}
+  alt="my profile"
+  className="rounded-full w-80 h-80 px-3 mt-20 ml-28 mr-14 flex sm:justify-self-start"
+/>
+
       </div>
     </div>
   )
