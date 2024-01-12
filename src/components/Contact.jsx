921e360a8c9420d7c44c20a5f212febd4e0d5204
sushantbro11 <sushantbro12@gmail.com>
2024-01-12 06:46:44 +0545

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,47 @@
-
-
 const Contact = () => {
   return (
-    <div className="text-white flex justify-center
-    items-center w-full h-screen">
-      <h1 className="text-4xl">Contact</h1>
-    </div>
-  )
-}
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white font-signature1"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8 mt-24">
+          <h1 className="py-6 text-3xl flex justify-center">Submit the form below to get in touch with me</h1>
+        </div>
 
-export default Contact
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/a4d115ce-e49c-44fd-a9b0-1319c96e6c7c"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
